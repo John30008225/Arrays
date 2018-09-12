d74d5debcb00cff98109c291ae1fb9aa19d15223
John Elder <30008225@student.toiohomai.ac.nz>
2018-09-13 09:15:48 +1200

    Arrays

---
+++ Arrays/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+
+            int i;
+            Console.Write("\n\nRead and Print elements of an array:\n");
+
+            Console.Write("Add 10 elements in the array :\n");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("Array {0} : ", i);
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("\nElements in array are: ");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("{0}  ", nums[i]);
+
+            }
+            Console.ReadLine();
+            Console.Write("\n");
+        }
+    }
+}
